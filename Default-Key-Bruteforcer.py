2c3ae19c7c7c6c5d661e546b4a0322ef4724cad6
matamorphosis <mbrittain15@gmail.com>
2018-08-13 09:06:54 +1000

    Add files via upload

--- Default-Key-Bruteforcer.py
+++ Default-Key-Bruteforcer.py
@@ -1,75 +1,79 @@
 #!/bin/python
 
-import RPi.GPIO as GPIO
-import MFRC522
-import signal
-import ast
-import sys
+import RPi.GPIO as GPIO, MFRC522, signal, ast, sys, time
 
 continue_reading = True
 
-# Capture SIGINT for cleanup when the script is aborted
+# Capture SIGINT for cleanup when the script is aborted.
 def end_read(signal,frame):
     global continue_reading
-    print "Ctrl+C captured, ending read."
+    print "[!] INTERRUPT [!] - Ctrl+C captured, stopping program and cleaning up."
     continue_reading = False
     GPIO.cleanup()
 
-# Hook the SIGINT
+# Hook the SIGINT.
 signal.signal(signal.SIGINT, end_read)
 
-# Create an object of the class MFRC522
+# Create an object of the class MFRC522.
 MIFAREReader = MFRC522.MFRC522()
 
-# Welcome message
+# Welcome message.
 print ("[i] RFID Key Bruteforcer.")
-print ("[i] Written by Matthew Brittain, Built on https://github.com/mxgxw/MFRC522-python.")
+print ("[i] Written by Matthew Brittain, built on https://github.com/mxgxw/MFRC522-python.")
 print ("[i] Press Ctrl-C to stop the program.")
 
-# This loop keeps checking for chips. If one is near it will get the UID and authenticate
+# This loop keeps checking for chips. If one is near it will get the UID and authenticate.
 while continue_reading:
-    
-    # Scan for cards    
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
-    # If a card is found
-    if status == MIFAREReader.MI_OK:
-        print ("[+] Card detected")
+    try:
+        # Import keys to test from text file.
+        with open('keys.txt') as lines:
+            newlines = lines.read().splitlines()
+    except:
+        sys.exit("[-] Failed to open file keys.txt.")
     
-    	# Get the UID of the card
-    	(status,uid) = MIFAREReader.MFRC522_Anticoll()
+    for newline in newlines:
+        # Scan for cards.
+        (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
-    	# If we have the UID, continue
-    	if status == MIFAREReader.MI_OK:
+        # If a card is found.
+        if status == MIFAREReader.MI_OK:
+            print("[+] Card tap-on detected.")
 
-        	# Print UID
-        	print ("[+] Card read UID: " + ",".join(uid))
-    
-        	# This is the default key for authentication
-        	# key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
-            try:
-                # Import UIDs from text file
-        	    lines = [line.rstrip('\n') for line in open('keys.txt')]
-            except:
-                sys.exit("[-] Failed to open file.")
+            # Get the UID of the card.
+            (status,uid) = MIFAREReader.MFRC522_Anticoll()
+
+            # If we have the UID, continue.
+            if status == MIFAREReader.MI_OK:
+
+                # Print UID
+                print("[+] Card read UID: " + ",".join(str(u) for u in uid) + ".")
+
+                # This is the default key for authentication:
+                # key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+
+                # Strip the imported UID of commas.
+                keya = [byte.strip() for byte in newline.split(',')]
+                keyanew = []
+                for k in keya:
+                    nk = k.replace(' ','')
+                    keyanew.append(nk)
+                key = [int(byte2,16) for byte2 in keyanew]
+                print("[i] Trying the key: " + str(key) + ". Please tap RFID card on now.")
                 
-            for l in lines:
-                # Strip the imported UID of commas
-                keya = [ byte.strip() for byte in l.split(',') ]
-            	    key = [int(byte,16) for byte in keya]
-            	    print ("[i] Trying the key: " + key)
+                # Select the scanned tag.
+                MIFAREReader.MFRC522_SelectTag(uid)
 
-            	    # Select the scanned tag
-            	    MIFAREReader.MFRC522_SelectTag(uid)
+                # Authenticate.
+                status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
 
-            	    # Authenticate
-            	    status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+                # Check if authenticated.
+                if status == MIFAREReader.MI_OK:
+                    MIFAREReader.MFRC522_Read(8)
+                    MIFAREReader.MFRC522_StopCrypto1()
+                    sys.exit("[!] SUCCESS [!] - The correct key is " + str(key))
 
-            	    # Check if authenticated
-            	    if status == MIFAREReader.MI_OK:
-                    	MIFAREReader.MFRC522_Read(8)
-                    	MIFAREReader.MFRC522_StopCrypto1()
-                    	print ("[+] The correct key is " + key)
-                
-                    else:
-               	    	print ("[-] Authentication error")
+                else:
+                    print("[-] Authentication error")
+        #else:
+            #print("[-] Card not detected.")
