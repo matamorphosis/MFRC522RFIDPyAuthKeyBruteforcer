f2f03d04e89e6ba1e1431ba7d6f477a8a4ec5967
matamorphosis <mbrittain15@gmail.com>
2018-08-23 18:31:13 +1000

    Update Dump.py

--- Dump.py
+++ Dump.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
-import RPi.GPIO as GPIO
-import MFRC522
-import signal
+import RPi.GPIO as GPIO, mfrc522, signal
 
 continue_reading = True
 
 # Capture SIGINT for cleanup when the script is aborted
-def end_read(signal,frame):
+def end_read(signal, frame):
     global continue_reading
     print "Ctrl+C captured, ending read."
     continue_reading = False
@@ -24,22 +22,22 @@ MIFAREReader = MFRC522.MFRC522()
 while continue_reading:
     
     # Scan for cards    
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+    (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
     # If a card is found
     if status == MIFAREReader.MI_OK:
         print "Card detected"
     
     # Get the UID of the card
-    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+    (status, uid) = MIFAREReader.MFRC522_Anticoll()
 
     # If we have the UID, continue
     if status == MIFAREReader.MI_OK:
 
         # Print UID
-        print "Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
+        print("[+] Card read UID: " + ",".join(str(u) for u in uid) + ".")
     
-        # This is the default key for authentication
+        # This is the default key for authentication, change once detected.
         key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
         
         # Select the scanned tag
